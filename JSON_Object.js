ee2abda74cb4331aa931f3ea7e60ce7dbacd2322
I. Temirkhaev <67521580+ckizp@users.noreply.github.com>
2022-12-07 15:51:58 +0100

    Update JSON_Object.js

--- JSON_Object.js
+++ JSON_Object.js
@@ -1,55 +1,51 @@
 const fs = require('fs');
 
 class JSON_Config {
-    constructor(name) {
-        this.name = name;
-        this.file = JSON.parse(fs.readFileSync(name));
-    }
+  constructor(name) {
+    this.name = name;
+    this.file = JSON.parse(fs.readFileSync(name));
+  }
 
-    addServer(server) {
-        if (!this.file.servers.forEach((array) => {
-            if (array.id == server.id)
-                return true;
-        })) {
-            this.file.servers.push(server.serv);
-            fs.writeFileSync(this.name, JSON.stringify(this.file, null, 3));
-        }
+  addServer(serv) {
+    let isNewServer = new Boolean(true);
+    this.file.servers.forEach((array) => {
+      isNewServer = (array.id === serv.id) ? (false) : (true)
+    })
+    if (isNewServer) {
+      this.file.servers.push({
+        id: serv.id,
+        welcomeBackgroundURL: "default.png"
+      });
+      fs.writeFileSync(this.name, JSON.stringify(this.file, null, 3));
     }
-
-    updateServer(serv, welcomeBackgroundURL) {
-        let index = this.file.servers.findIndex(server => server.id = serv.id);
-        this.file.servers[index].welcomeBackgroundURL = welcomeBackgroundURL;
-        fs.writeFileSync(this.name, JSON.stringify(this.file, null, 3));
-    }
+  }
 }
 
 class Server {
-    constructor(server_id) {
-        this.serv = {
-            id : server_id,
-            welcomeBackgroundURL: "blue-lock.png"
-        }
-    }
+  constructor(id) {
+    this.id = id;
+  }
 }
 
 const obj = new JSON_Config("config.json");
-
 const serv = new Server("647800671218958336");
 obj.addServer(serv);
 
-obj.updateServer(serv, "bluck-lock2.png");
+/*  addServer(server) {
+    var isNewServer = true;
+    this.file.servers.includes((obj) => {
+      if (obj.id = server.id)
+        isNewServer = false;
+    })
+    if (isNewServer) {
+      this.file.servers.push(server.getServer());
+      fs.writeFileSync(this.name, JSON.stringify(this.file, null, 3));
+    }
+  }
 
-
-
-/*fs.readFile('config.json', (err, data) => {
-    let obj = JSON.parse(data);
-
-    obj.servers.push({
-        name: "test",
-        desc: "test"
-    });
-
-    let json = JSON.stringify(obj, null, 3);
-
-    fs.writeFile('config.json', json, (err_) => {});
-});*/
+  updateServer(serv, bgURL) {
+    let index = this.file.servers.findIndex(server => server.id = serv.id);
+    //this.file.servers[index].welcomeBackgroundURL
+    console.log(this.file)
+    fs.writeFileSync(this.name, JSON.stringify(this.file, null, 3));
+  }*/
