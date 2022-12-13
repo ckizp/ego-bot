edb996c364013232f6cbafed6be1f9c7636f0c55
I. Temirkhaev <67521580+ckizp@users.noreply.github.com>
2022-12-13 23:55:30 +0100

    add kick and ping commands

---
+++ commands/egkick.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('egkick')
+        .setDescription('kick a user from the server')
+        .addUserOption((option) => option.setName('member').setDescription('Member to kick'))
+        .addStringOption((option) => option.setName('reason').setDescription('State the reason')),
+        /**
+         * @param {ChatInputCommandInteraction} interaction
+         */
+        async execute(interaction) {
+            const member = interaction.options.get('member');
+            const reason = interaction.options.get('reason')
+            
+            if(!interaction.member.permissions.has('KICK_MEMBERS')) {
+                interaction.reply('Vous n\'avez pas la permission d\'exécuter cette command !');
+                return;
+            }
+
+            member.kick(reason)
+        }
+}
