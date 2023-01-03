ab4606845ecfc50f364afd003a5bab1b4c9523b8
I. Temirkhaev <67521580+ckizp@users.noreply.github.com>
2023-01-03 14:57:15 +0100

    Create search.js

---
+++ functions/search.js
@@ -0,0 +1,50 @@
+const dictionary = [];
+
+function getClosestWord(word) {
+  let minDistance = Number.MAX_VALUE;
+  let closestWord = '';
+  for (let i = 0; i < dictionary.length; i++) {
+    const distance = levenshteinDistance(word, dictionary[i]);
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestWord = dictionary[i];
+    }
+  }
+  return closestWord;
+}
+
+function levenshteinDistance(a, b) {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+
+  let matrix = [];
+
+  // Initialisation de la première ligne et de la première colonne de la matrice
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  // Remplissage de la matrice
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // substitution
+          matrix[i][j - 1] + 1, // insertion
+          matrix[i - 1][j] + 1, // suppression
+        );
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+}
+
+
+const userInput = '';
+console.log(getClosestWord(userInput));
